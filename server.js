baa3eff25ae94d47538e0ed9d9b4c106f3340829
V-allary <vallarymitchelle4@gmail.com>
2026-01-08 21:48:43 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -23,15 +23,12 @@ app.post("/submit-form", async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT),
-      secure: false,  
+      secure: Number(process.env.SMTP_PORT) === 465, 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      tls: {
-        rejectUnauthorized: false
-      }
-    });
+    }); 
     await transporter.sendMail({
       from: `"MWUAP Website" <${process.env.EMAIL_USER}>`,
       to: process.env.EMAIL_USER,
